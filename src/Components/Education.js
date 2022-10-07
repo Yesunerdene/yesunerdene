15221f3134f8ed88456e25f6251e5f7fa4250c96
yesunerdene <yesuuygls@gmail.com>
2022-10-07 12:53:27 +0800

    fix: :zap: reduces image asset sizes

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -8,12 +8,8 @@ function Education() {
       {config?.educations?.map((item) => (
         <div key={item?.from}>
           <p className="title text-small-gray">{`${item.from} - ${item.to}`}</p>
-          <p className="title text-bigger mb0">{item.major}</p>
-          <a
-            target="_blank"
-            className="title text-bigger mb2"
-            href={item?.href}
-          >
+          <p className="title mb0">{item.major}</p>
+          <a target="_blank" className="title mb2" href={item?.href}>
             {item.university}
           </a>
           <br />
