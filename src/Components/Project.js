3e24295309d4841c97d9f0070176651a24d8412a
yesunerdene <yesuuygls@gmail.com>
2022-12-10 12:15:50 +0800

    fix: :memo: fixed education section

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -4,22 +4,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Row, Col, Image, Modal, Container } from "react-bootstrap";
 
 function Project({ theme }) {
-  const [selected, select] = useState(null);
+  const [selected, select] = useState(config?.projects[0]);
 
   return (
     <div className="my-card my-info">
       <p className="card-title">Projects</p>
+      <p className="title text-bigger mb0 ml1">{`Mobile applications`}</p>
       <Row className="my-row">
         {config?.projects?.map((item) => (
-          <Col lg={3} md={6} sm={12} xs={12} key={item?.title} className="">
-            <div key={item?.title} className="project-holder">
+          <Col
+            lg={4}
+            md={6}
+            sm={12}
+            xs={12}
+            key={item?.title}
+            // onClick={() => select(item)}
+          >
+            <div
+              key={item?.title}
+              className={`project-holder 
+                // {selected?.title === item?.title ? "selected-project" : ""}
+              `}
+            >
               <Image src={item?.logo} className="project-logo mb2" />
               <p className="title text-bigger mb0 text-center">{item.title}</p>
-              <p className="title text-center mb0">{`by ${item.company}`}</p>
+              <p className="title text-center mb0">{`by ${
+                item.company || ""
+              }`}</p>
               <p className="title text-small-gray text-center">{`released on ${item.releasedOn}`}</p>
               <div className="my-row2 mt1 ">
                 <div className="mr3">
-                  <a target="blank" href={item?.appStore}>
+                  <a target="blank" href={item?.appStore || ""}>
                     <FontAwesomeIcon
                       size="3x"
                       // style="--fa-animation-duration: 0.5s;"
@@ -29,7 +44,7 @@ function Project({ theme }) {
                   </a>
                 </div>
                 <div className="ml3">
-                  <a target="blank" href={item?.playStore}>
+                  <a target="blank" href={item?.playStore || ""}>
                     <FontAwesomeIcon
                       size="3x"
                       // style="--fa-animation-duration: 0.5s;"
@@ -45,20 +60,64 @@ function Project({ theme }) {
           </Col>
         ))}
       </Row>
-      <Modal show={selected?.id === 0} onHide={null}>
+      <p className="title text-bigger mb0 ml1">{`Web sites`}</p>
+      <Row className="my-row">
+        {config?.webs?.map((item) => (
+          <Col
+            lg={4}
+            md={6}
+            sm={12}
+            xs={12}
+            key={item?.title}
+            // onClick={() => select(item)}
+          >
+            <div
+              key={item?.title}
+              className={`project-holder 
+                // {selected?.title === item?.title ? "selected-project" : ""}
+              `}
+            >
+              <Image src={item?.logo} className="project-logo mb2" />
+              <p className="title text-bigger mb0 text-center">{item.title}</p>
+              <p className="title text-center mb0">{`by ${
+                item.company || ""
+              }`}</p>
+              <p className="title text-small-gray text-center">{`released on ${item.releasedOn}`}</p>
+              <div className="my-row2 mt1 ">
+                <div className="mr3">
+                  <a target="blank" href={item?.link || ""}>
+                    <FontAwesomeIcon
+                      size="3x"
+                      // style="--fa-animation-duration: 0.5s;"
+                      icon="fa-solid fa-link"
+                      color={theme === "dark-theme" ? "#FFF" : "#2a303c"}
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="line" />
+              <div className="dot" />
+            </div>
+          </Col>
+        ))}
+      </Row>
+      {/* <div>
+        <p className="title text-bigger mb0 text-center">{selected.title}</p>
+      </div> */}
+      {/* <Modal show={selected?.id === 0} onHide={null}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>Woohoo, youre reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-          {/* <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
           <Button variant="primary" onClick={handleClose}>
             Save Changes
-          </Button> */}
+          </Button>
         </Modal.Footer>
-      </Modal>
+      </Modal> */}
     </div>
   );
 }
--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ module.exports = {
   profession: "Software Engineer",
   occupation: "Mobile app developer",
   description:
-    "Experienced mobile application developer with 3 years of experience and more other software development in the IT industry in Mongolia. Professional at whole  process of mobile application development from the blank project to release and launch to public application stores and the maintenance.",
+    "Passionate software engineer, especially experienced in mobile application development for 3.5 years and other software development in the IT industry in Mongolia. Professional at the whole mobile application development process from the blank project to release and launch to public application stores and the maintenance, which includes designing, developing and testing. And I'm very keen to study advanced computer science, especially artificial intelligence and data science.",
   contact: [
     {
       key: "Email",
@@ -39,9 +39,14 @@ module.exports = {
   educations: [
     {
       href: "https://www.google.com/search?q=National+University+of+Mongolia&sxsrf=ALiCzsZeBFmdyE8Rb9QRLBJY8dt4SK4_pA%3A1665033815538&ei=V2Y-Y9S4IIi52roP7_-ymAI&ved=0ahUKEwiUwPSo7sr6AhWInFYBHe-_DCMQ4dUDCA4&uact=5&oq=National+University+of+Mongolia&gs_lcp=Cgdnd3Mtd2l6EAMyCggAEEcQ1gQQsAMyCggAEEcQ1gQQsAMyCggAEEcQ1gQQsAMyCggAEEcQ1gQQsAMyCggAEEcQ1gQQsAMyCggAEEcQ1gQQsAMyCggAEEcQ1gQQsAMyCggAEEcQ1gQQsAMyBwgAELADEEMyBwgAELADEEMyBwgAELADEEMyBwgAELADEEMyDQgAEOQCENYEELADGAEyDQgAEOQCENYEELADGAEyDQgAEOQCENYEELADGAEyDAguEMgDELADEEMYAjIMCC4QyAMQsAMQQxgCMgwILhDIAxCwAxBDGAIyDAguEMgDELADEEMYAkoECEEYAEoECEYYAVDJAljJAmDyA2gDcAF4AIABAIgBAJIBAJgBAKABAqABAcgBE8ABAdoBBggBEAEYCdoBBggCEAEYCA&sclient=gws-wiz",
-      university: "National University of Mongolia 🇲🇳,",
+      university: "National University of Mongolia 🇲🇳",
       department:
         "\nSchool of Engineering and Applied Scince, (Department of Information and Computer Science)",
+      thesis:
+        "The real estate advertisement for renting or selling platform based on a map.",
+      thesisDescription:
+        "Based on the thesis, I developed a mobile application and a website for a real estate platform.",
+      grade: "3.5 / 4.0",
       major: "Software Engineer",
       degree: "Bachelor",
       from: "2016",
@@ -71,13 +76,13 @@ module.exports = {
   ],
   experiences: [
     {
-      company: "Octagon NFT LLC",
+      company: "Octagon LLC",
       position: "Software Developer - Mobile Application Developer",
       from: "Jan 2022",
       to: "present",
       href: "https://powered.mn/",
       description:
-        "Working as software developer, especially in mobile application development on OCTAGON.MN - NFT marketplace and a brige to connect to crypto wallets across the world",
+        "Octagon LLC is a technology company founded in December 2021 which runs the Octagon - a crypto-based NFT marketplace and crypto exchange platform. \n\n I have been working as a software engineer, especially in the mobile application development of the Octagon app, which includes features of a digital wallet control, a crypto exchange and an NFT marketplace. As a developer, I take responsibility for the whole mobile application project, which I do project planning, designing, developing, testing and maintaining the app.",
     },
     {
       company: "Powered LLC",
@@ -86,7 +91,7 @@ module.exports = {
       to: "present",
       href: "https://powered.mn/",
       description:
-        "Working as software developer, especially in mobile application development on YESH.MN -  Test based learning app for university general entrance examination for high school students.",
+        "Powered LLC is a technology company founded in January 2021 which runs the YESH.MN - test- based learning app for university general entrance examination for high school students.\n\n I have been working as a software engineer, especially in the mobile application development of YE SH.MN app, which includes features for taking a test of entrance exam. I take responsibility for the whole mobile application project, which involves project planning, designing, developing, testing and maintaining the app.",
     },
     {
       company: "Digital Mall LLC",
@@ -95,7 +100,7 @@ module.exports = {
       to: "Jan 2022",
       href: "https://www.bananamall.mn/",
       description:
-        "Worked as software developer, mainly in mobile application development, also in e-commerce platform core backend development, and as database engineer on BananaMall.mn - ecommerce software.",
+        "Digital Mall LLC is a technology company founded in June 2019 which runs the BananaMall - an e- commerce platform that for online shopping with delivery service.\n\nAt the beginning of my 4th year of study in software engineering, my former internship employer company offered me to work as a software developer in their e-commerce system. As a part of the development team, I worked as a software engineer, especially in the development of the mobile application, core backend development, and admin panel of the e-commerce platform, and took responsibility for database engineering.",
     },
     {
       company: "Alpha Labs LLC",
@@ -104,14 +109,34 @@ module.exports = {
       to: "Sep 2022",
       href: "https://www.bsb.mn/",
       description:
-        "Worked as software developer, in mobile application development on BSB.mn - e-commerce platform, sells technological and furniture products.",
+        "Alpha Labs LLC is a technology company founded in January 2015 and conducts the development of various kinds of platforms. \n\n I have been working as a software engineer, especially in the mobile application development of BS B.MN, G-Power monitoring and ISU Mongolia. I take responsibility in whole projects of the mobile applications respectively, in which I do project planning, designing, developing, testing and maintaining the app.",
+    },
+  ],
+  webs: [
+    {
+      id: 0,
+      title: "BananaMall",
+      company: "Digital Mall LLC",
+      role: "Software engineer",
+      releasedOn: "2019.11.01",
+      link: "https://bananamall.mn",
+      logo: require("./assets/bananamall.png"),
+    },
+    {
+      id: 1,
+      title: "Togloom Trade",
+      company: "Ocurus.mn",
+      role: "Mobile app developer",
+      releasedOn: "2022.03.10",
+      link: "https://togloom.trade",
+      logo: require("./assets/togloom.png"),
     },
   ],
   projects: [
     {
       id: 0,
       title: "Octagon Wallet",
-      company: "Octagon NFT LLC",
+      company: "Octagon LLC",
       role: "Mobile app developer",
       releasedOn: "2022.03.10",
       appStore: "https://apps.apple.com/mn/app/octagon-wallet/id1611280720",
@@ -149,6 +174,26 @@ module.exports = {
       playStore:
         "https://play.google.com/store/apps/details?id=com.bsb.mobileapp",
       logo: require("./assets/bsb.png"),
+    },
+    {
+      id: 4,
+      title: "ISU Mongolia",
+      // company: "Alpha Labs LLC",
+      role: "Mobile app developer",
+      releasedOn: "coming soon",
+      appStore: "",
+      playStore: "",
+      logo: require("./assets/isu.jpeg"),
+    },
+    {
+      id: 5,
+      title: "G-Power monitoring",
+      // company: "Alpha Labs LLC",
+      role: "Mobile app developer",
+      releasedOn: "coming soon",
+      appStore: "",
+      playStore: "",
+      logo: require("./assets/gpower.png"),
     },
   ],
   certificates: [
