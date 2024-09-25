ba4cd8a7ca51ec499e981c65a8deac40b347e768
yesunerdene <yesuuygls@gmail.com>
2024-09-25 23:38:34 +0200

    feat: :sparkles: added my new study

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -6,7 +6,7 @@ function Education() {
     <div className="my-card my-info">
       <p className="card-title">Education</p>
       {config?.educations?.map((item) => (
-        <div key={item?.from}>
+        <div key={item?.from} className="mt1">
           <p className="title text-small-gray">{`${item.from} - ${item.to}`}</p>
           <p className="title mb0">{item.major}</p>
           <a target="_blank" className="title mb2" href={item?.href}>
--- src/Components/Project.js
+++ src/Components/Project.js
@@ -8,58 +8,74 @@ function Project({ theme }) {
 
   return (
     <div className="my-card my-info">
-      <p className="card-title">Projects</p>
-      <p className="title text-bigger mb0 ml1">{`Mobile applications`}</p>
+      <p className="card-title">Projects / Mobile applications</p>
+      {/* <p className="title text-bigger mb0 ml1">{``}</p> */}
       <Row className="my-row">
-        {config?.projects?.map((item) => (
-          <Col lg={3} md={6} sm={12} xs={12} key={item?.title}>
-            <div
-              key={item?.title}
-              className={`project-holder 
-                ${selected?.title === item?.title ? "selected-project" : ""}
-              `}
-            >
-              <Image src={item?.logo} className="project-logo mb2" />
-              <p className="title text-bigger mb0 text-center">{item.title}</p>
-              <p className="title text-center mb0">{`by ${
-                item.company || ""
-              }`}</p>
-              <p className="title text-small-gray text-center">{`released in ${item.releasedOn}`}</p>
-              <div className="my-row2 mt1 ">
-                <div className="mr3">
-                  <a target="blank" href={item?.appStore || ""}>
-                    <FontAwesomeIcon
-                      size="2xl"
-                      // style="--fa-animation-duration: 0.5s;"
-                      icon="fa-brands fa-app-store fa-beat"
-                      color={theme === "dark-theme" ? "#FFF" : "#535c6e"}
-                    />
-                  </a>
-                </div>
-                <div className="ml3">
-                  <a target="blank" href={item?.playStore || ""}>
-                    <FontAwesomeIcon
-                      size="2xl"
-                      // style="--fa-animation-duration: 0.5s;"
-                      icon="fa-brands fa-google-play fa-beat"
-                      color={theme === "dark-theme" ? "#FFF" : "#535c6e"}
-                    />
-                  </a>
-                </div>
-                <div className="ml3" onClick={() => select(item)}>
-                  <p className="title text-bigger2 mb0 text-center">ⓘ</p>
-                </div>
-              </div>
-              <div className="line" />
-              <div className="dot" />
-            </div>
-          </Col>
-        ))}
+        {config?.projects?.map((project) => {
+          return (
+            <Col lg={3} md={6} sm={12} xs={12}>
+              <p className="title mt1">{project?.type}</p>
+              {project?.items?.map((item) => (
+                <Col lg={12} md={9} sm={12} xs={12} key={item?.title}>
+                  <Row
+                    key={item?.title}
+                    className={`project-holder my-row
+                    ${selected?.title === item?.title ? "selected-project" : ""}
+                  `}
+                  >
+                    <Col lg={3} md={9} sm={3} xs={3}>
+                      <Image src={item?.logo} className="project-logo mb2" />
+                    </Col>
+                    <Col lg={9} md={3} sm={3} xs={9}>
+                      <p className="title text-bigger mb0 text-center">
+                        {item.title}
+                      </p>
+                      {/* <p className="title text-center mb0">{`by ${
+                        item.company || ""
+                      }`}</p> */}
+                      {/* <p className="title text-small-gray text-center">{`released in ${item.releasedOn}`}</p> */}
+                      <div className="my-row2 mt1 ">
+                        <div className="mr3">
+                          <a target="blank" href={item?.appStore || ""}>
+                            <FontAwesomeIcon
+                              size="lg"
+                              icon="fa-brands fa-app-store fa-beat"
+                              color={
+                                theme === "dark-theme" ? "#FFF" : "#535c6e"
+                              }
+                            />
+                          </a>
+                        </div>
+                        <div className="ml2">
+                          <a target="blank" href={item?.playStore || ""}>
+                            <FontAwesomeIcon
+                              size="lg"
+                              // style="--fa-animation-duration: 0.5s;"
+                              icon="fa-brands fa-google-play fa-beat"
+                              color={
+                                theme === "dark-theme" ? "#FFF" : "#535c6e"
+                              }
+                            />
+                          </a>
+                        </div>
+                        <div className="ml3" onClick={() => select(item)}>
+                          <p className="title text-small mb0 text-center">ⓘ</p>
+                        </div>
+                      </div>
+                    </Col>
+                  </Row>
+                  <div className="line" />
+                  <div className="dot" />
+                </Col>
+              ))}
+            </Col>
+          );
+        })}
       </Row>
       <p className="title text-bigger mb0 ml1">{`Web sites`}</p>
       <Row className="my-row">
         {config?.webs?.map((item) => (
-          <Col lg={3} md={6} sm={12} xs={12} key={item?.title}>
+          <Col lg={3} md={6} sm={12} xs={12} key={`Projects / ${item?.title}`}>
             <div
               key={item?.title}
               className={`project-holder ${
@@ -73,7 +89,7 @@ function Project({ theme }) {
                 item.company || ""
               }`}</p>
               <p className="title text-small-gray text-center">{`released in ${item.releasedOn}`}</p>
-              <div className="my-row2 mt1 ">
+              <div className="my-row2  mb1">
                 <div className="mr3">
                   <a target="blank" href={item?.link || ""}>
                     <FontAwesomeIcon
